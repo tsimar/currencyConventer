4c2a5d3f43648244d6e4d49537b301bc804984ed
Volodymyr <coremio3@gmail.com>
2022-06-05 23:54:42 +0200

    checking my desing

--- js/ind.js
+++ js/ind.js
@@ -1,21 +1,37 @@
-const inp = document.querySelector("#inp");
+const input = document.querySelector("#in-money");
 const res1 = document.createElement("p");
+res1.classList.add("converter-total");
+const div = document.querySelector(".convert-container");
 let date = new Date();
 
-function convert(ur) {
-  fetch(ur)
+function checkInMoney(money) {
+  console.log(typeof money);
+  if (isNaN(money)) {
+    input.value = 0;
+    return 0;
+  } else if (money < 0) {
+    return money * -1;
+  }
+  return money;
+}
+
+function convert(url) {
+  fetch(url)
     .then((res) => res.json())
     .then((out) => {
-      res1.textContent = out.rates[0].ask * parseFloat(inp.value);
+      let formatNumber = (
+        out.rates[0].ask * checkInMoney(parseFloat(input.value))
+      ).toFixed(2);
+      res1.textContent = "to  " + formatNumber + " PLN";
       console.log(res1.textContent);
-      document.body.appendChild(res1);
+      div.appendChild(res1);
     })
     .catch((err) => console.error(err));
 }
 
 const btn = document.querySelector("#btn");
-const sb = document.querySelector("#framework");
-function plusZero(date) {
+const sb = document.querySelector("#currency");
+function addZero(date) {
   if (date < 10) {
     return (date = "0" + date);
   }
@@ -25,15 +41,15 @@ function plusZero(date) {
 btn.onclick = (event) => {
   event.preventDefault();
   let day = date.getDate();
-  let month = plusZero(date.getMonth() + 1);
+  let month = addZero(date.getMonth() + 1);
   if (date.getDay() === 0) {
     day = day - 2;
   } else if (date.getDay() === 6) {
     day = day - 1;
   }
-  day = plusZero(day);
+  day = addZero(day);
   let dateByConvert = date.getFullYear() + "-" + month + "-" + day;
 
-  let ur = `http://api.nbp.pl/api/exchangerates/rates/c/${sb.value}/${dateByConvert}/?format=json`;
-  convert(ur);
+  let url = `http://api.nbp.pl/api/exchangerates/rates/c/${sb.value}/${dateByConvert}/?format=json`;
+  convert(url);
 };
